1eaa61b084f27636963092ce12b6bac50c93e308
HelgaN <ola14@tut.by>
2019-04-02 16:27:29 +0300

    components

---
+++ my-project/src/containers/Quiz/Quiz.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import './Quiz.css';
+import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz';
+
+class Quiz extends Component {
+  state = {
+    quiz: [
+      {
+        answers: [
+          { text: "Вопрос 1"},
+          { text: "Вопрос 2"},
+          { text: "Вопрос 3"},
+          { text: "Вопрос 4"}
+        ]
+      }
+    ]
+  }
+
+  render() {
+    return (
+      <div className="quiz">
+        <div className="quiz-wrapper">
+          <h1>Ответьте на все вопросы</h1>
+          <ActiveQuiz
+            answers={this.state.quiz[0].answers}
+          />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Quiz;
